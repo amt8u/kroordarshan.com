869d9b21c5c138ab842d55040efe9c8d22d7f9e1
amt8u <59372411+amt8u@users.noreply.github.com>
2024-11-23 14:51:16 +0530

    Updated index header and body

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kroordarshan.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Kroordarshan" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         Domain owned by <a href="mailto:hi@cybercafe.dev">amt8u</code>
         </p>
       </main>
 
